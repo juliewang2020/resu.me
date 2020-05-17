cdd4048aa12c2e32efbfb6320259e66025c79fed
Julie Wang <julie.wang@emory.edu>
2020-05-16 22:08:09 -0400

    added pics of nlp example

--- server.js
+++ server.js
@@ -17,32 +17,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/home.html'));
 });
 
-// Bucket Upload
+// 
 
+function logPaste(event) {
+  log.innerText = 'Analyzing!\n' + log.innerText;
+}
 
-// Run NLP Analysis
-async function quickstart() {
-    // Imports the Google Cloud client library
-    const language = require('@google-cloud/language');
-  
-    // Instantiates a client
-    const client = new language.LanguageServiceClient();
-  
-    // The text to analyze
-    const text = 'Hello, world!';
-  
-    const document = {
-      content: text,
-      type: 'PLAIN_TEXT',
-    };
-  
-    // Detects the sentiment of the text
-    const [result] = await client.analyzeSentiment({document: document});
-    const sentiment = result.documentSentiment;
-  
-    console.log(`Text: ${text}`);
-    console.log(`Sentiment score: ${sentiment.score}`);
-    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-  }
+const editor = document.getElementById('editor');
+const log = document.getElementById('log');
 
-
+editor.oncopy = logCopy;
+editor.onpaste = logPaste;
